8c14bafd2a7fba9523afba7ad7a4f8cb0acb3d1f
d0minikk <curylo.dominik@gmail.com>
2018-04-22 20:42:14 +0200

    Add proper associations to Menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,10 @@
 class Menu < ApplicationRecord
   has_many :menu_pages
+
+  belongs_to :sponsor, optional: true
+  belongs_to :event, optional: true
+  belongs_to :venue, optional: true
+  belongs_to :place, optional: true
+  belongs_to :occasion, optional: true
+  belongs_to :location, optional: true
 end
