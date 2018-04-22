a76ef6fb6752207b7885dad17a0053fb8e5a062c
d0minikk <curylo.dominik@gmail.com>
2018-04-22 21:53:00 +0200

    Set optional as true for MenuPage and MenuItem association

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,4 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu_page
-  belongs_to :dish
+  belongs_to :dish, optional: true
 end
--- app/models/menu_page.rb
+++ app/models/menu_page.rb
@@ -2,5 +2,5 @@ class MenuPage < ApplicationRecord
   has_many :menu_items
   has_many :dishes, through: :menu_items
 
-  belongs_to :menu
+  belongs_to :menu, optional: true
 end
