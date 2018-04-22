15a7afdfad9fb2d33a3b253a158aab763dd78905
d0minikk <curylo.dominik@gmail.com>
2018-04-22 20:55:47 +0200

    Add few views

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -2,4 +2,8 @@ class OccasionsController < ApplicationController
   def index
     @occasions = Occasion.all.page(params[:page])
   end
+
+  def show
+    @occasion = Occasion.find(params[:id])
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,8 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all.page(params[:page])
   end
+
+  def show
+    @place = Place.find(params[:id])
+  end
 end
